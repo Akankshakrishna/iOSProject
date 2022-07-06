9e78d4ab6c4c41d7b00d3209437f805ff36190ea
Akankshakrishna <akanksha36200@gmail.com>
2022-07-06 14:49:22 +0530

    Initial Commit

---
+++ Astro Torch/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Astro Torch
+//
+//  Created by Akanksha.A on 28/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var screenRed = true
+    
+    func makeViewRed(){
+        self.view.backgroundColor = .green
+    }
+    func makeViewBlue(){
+        self.view.backgroundColor = .yellow    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func colorChange(_ sender: UIButton) {
+        if screenRed{
+            makeViewBlue()
+        }
+        else {
+            makeViewRed()
+        }
+        screenRed = !screenRed
+    }
+}
+
